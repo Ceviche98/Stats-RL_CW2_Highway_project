9ef92cb4776b0e677facc3fdce70e87f0b89b4c4
Sriram R <140894396+SriramRajarajan@users.noreply.github.com>
2026-01-13 16:19:39 +0000

    Update QRDQN_PATH for experiment 4

--- experiments/exp4/exp4_blind_spot_video.py
+++ experiments/exp4/exp4_blind_spot_video.py
@@ -9,7 +9,7 @@ from gymnasium.wrappers import RecordVideo
 # --- CONFIGURATION ---
 USE_SAFETY_SHIELD = False  
 DQN_PATH = "../../models/exp1/E_Low_freq_aggr_DQN_s200.zip"
-QRDQN_PATH = "../../models/exp1_aggressive_QRDQN_s200.zip"
+QRDQN_PATH = "../../models/exp1/F_Low_freq_aggr_QRDQN_s200.zip"
 VIDEO_DIR = "../../videos/exp4_blind_spot/"
 
 os.makedirs(VIDEO_DIR, exist_ok=True)
